19b871ec638bbe910a6e46c70a52ddf1b2f83213
dajigin <dajigin@mail.ru>
2022-09-17 17:36:19 +0300

    Added script for creating LibreOffice SDK libraries symbolic links in /usr/lib64

---
+++ loffice_libs.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Создание символических ссылок на библиотеки LibreOffice SDK
+
+# Поменяйте на свой путь к LibreOffice SDK:
+export OFFICE_SDK=/usr/lib64/libreoffice/sdk
+##########################################
+
+if [ "$USER" != "root" ]
+then
+    echo 'Ошибка: скрипт должен быть запущен от имени root. Используйтe команду для запуска:
+sudo ./loffice_libs.sh'
+    exit
+fi
+
+ln -s -t /usr/lib64/ $OFFICE_SDK/lib/libuno_cppu.so
+ln -s -t /usr/lib64/ $OFFICE_SDK/lib/libuno_sal.so
+ln -s -t /usr/lib64/ $OFFICE_SDK/lib/libuno_cppuhelpergcc3.so
